8fec50320f52d6173809100fa216cf30e41eb345
Jerry Kim <jerryrox@live.co.kr>
2024-05-18 13:41:38 +1200

    Fixed useAsyncEffect not exposed

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export { default as Gap } from "./components/Gap";
 export { default as Grid } from "./components/Grid";
 export { default as Row } from "./components/Row";
 
+export { useAsyncEffect } from "./hooks/useAsyncEffect";
 export { useBindable, useBindableUnsafe } from "./hooks/useBindable";
 export { useEventAction } from "./hooks/useEventAction";
